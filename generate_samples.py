aaef9cae518a2864f7ba7ceafa4541d17a7958ad
pedromartins <pedro.martins@dtx-colab.pt>
2025-06-02 14:30:13 +0100

    fix print

--- generate_samples.py
+++ generate_samples.py
@@ -372,7 +372,7 @@ def generate_samples(
             print(f"Sample {sample_idx}/{max_samples} complete", " " * 200, end="\r")
 
         # Next batch
-        _LOGGER.debug("Batch %s/%s complete", batch_idx + 1, max_samples // batch_size, end="\r")
+        print("Batch %s/%s complete", batch_idx + 1, max_samples // batch_size, end="\r")
         speakers_batch = list(it.islice(speakers_iter, 0, batch_size))
         batch_idx += 1
 
